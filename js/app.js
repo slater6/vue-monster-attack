23042b9914e579861dad004d54fc44e956765524
slater6 <slaters@gmail.com>
2017-03-27 21:59:32 -0400

    Finished Monster Slayer Tutorial

--- js/app.js
+++ js/app.js
@@ -3,16 +3,21 @@ new Vue({
   data:{
     playerHealth:100,
     monsterHealth:100,
-    gameIsRunning: false
+    gameIsRunning: false,
+    turns: []
   },
   methods:{
     startGame: function(){
       this.gameIsRunning = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
+      this.turns = [];
     },
     attack:function(){
-      this.monsterHealth -= this.calculateDamage(3,10);
+      let damage = this.calculateDamage(3,10);
+      this.monsterHealth -= damage
+
+      this.log(true,damage);
 
       if(!this.checkWin()){
         this.monsterAttack();
@@ -20,7 +25,11 @@ new Vue({
 
     },
     specialAttack:function(){
-      this.monsterHealth -= this.calculateDamage(10,20);
+      let damage =  this.calculateDamage(10,20);
+
+      this.monsterHealth -= damage;
+
+      this.log(true,damage);
 
       if(!this.checkWin()){
           this.monsterAttack();
@@ -38,11 +47,19 @@ new Vue({
       this.gameIsRunning = false;
     },
     monsterAttack(){
-      this.playerHealth -= this.calculateDamage(5,12);
+      let damage =  this.calculateDamage(5,12);
+      this.playerHealth -= damage;
+      this.log(false,damage);
       this.checkWin();
     },
     calculateDamage: function(min,max){
         return Math.max(Math.floor(Math.random() * max),min);
+    },
+    log: function(isPlayer,damage){
+      this.turns.unshift({
+        isPlayer,
+        text: (isPlayer) ? 'Player hits Monster for ' + damage : 'Monster hits player for ' + damage
+      });
     },
     checkWin:function(){
       if(this.monsterHealth <= 0){
