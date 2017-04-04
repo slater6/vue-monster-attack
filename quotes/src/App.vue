01bee8ce7560f5de235580431cb7f6509c099f19
Bryan <slaters@gmail.com>
2017-04-04 14:19:24 -0400

    Quotes : Added new components

--- quotes/src/App.vue
+++ quotes/src/App.vue
@@ -1,14 +1,24 @@
 <template>
-    <div class="container">
-    
-    </div>
+<div class="container">
+
+</div>
 </template>
 
 <script>
-    export default {
-        
+export default {
+    data: function() {
+        return {
+            quotes: [
+                'Just a Quote to see something'
+            ],
+            maxQuotes: 10
+        }
+
     }
+
+}
 </script>
 
 <style>
+
 </style>
